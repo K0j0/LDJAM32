452e48c676d93575dfc84482ec5be83bc3fc735d
Kojo Kumah <info@kojokumah.com>
2015-04-17 22:42:47 -0700

    Started getting character to move

---
+++ Assets/Scripts/HelperFunctions.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelperFunctions : MonoBehaviour {
+
+    static HelperFunctions instance;
+
+    public void Awake()
+    {
+        instance = this;
+    }
+
+    public delegate void Callback();
+    public static void DelayCallback(float delay, Callback callback)
+    {
+        instance.StartCoroutine(DelayCallbackCoroutine(delay, callback));
+    }
+
+    static IEnumerator DelayCallbackCoroutine(float delay, Callback callback)
+    {
+        yield return new WaitForSeconds(delay);
+        callback();
+    }
+
+    public static void tick(float duration, int count, Callback callback, bool doNow = false)
+    {
+        instance.StartCoroutine(tickCoroutine(duration, count, callback, doNow));
+    }
+
+    static IEnumerator tickCoroutine(float delay, int count, Callback callback, bool doNow = false)
+    {
+        if (doNow) callback();
+
+        while (count-- >= 0)
+        {
+            yield return new WaitForSeconds(delay);
+            callback();
+        }
+    }
+
+    public static void waitLoadScene(float waitTime, string sceneName)
+    {
+        instance.StartCoroutine(waitLoadSceneRoutine(waitTime, sceneName));
+    }
+
+    static IEnumerator waitLoadSceneRoutine(float waitTime, string sceneName)
+    {
+        yield return new WaitForSeconds(waitTime);
+        Application.LoadLevel(sceneName);
+    }
+}
---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hero : MonoBehaviour {
+    public float speed = 5;
+    float multiplier = 100;
+
+    float adjSpeed
+    {
+        get
+        {
+            return speed / multiplier;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 currPos = transform.localPosition;
+
+	    // movement
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            print("H+");
+            currPos.x += adjSpeed;
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            print("H-");
+            currPos.x -= adjSpeed;
+        }
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            print("V+");
+            currPos.y += adjSpeed;
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            print("V-");
+            currPos.y -= adjSpeed;
+        }
+        transform.localPosition = currPos;
+	}
+}
