ad941bd6a53bdb6975db7e45f851938919a7db1b
Kojo Kumah <info@kojokumah.com>
2015-04-18 15:58:38 -0700

    Added npc and simple animation

---
+++ Assets/MainScript.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainScript : MonoBehaviour {
+    public Hero hero;
+    public Camera cam;
+    float camLeftBound = 0;
+    float camRightBound = 10;
+    float heroTopBounds = 0;
+    float heroBottomBounds = -3.6f;
+    float heroFeetTopBounds = -.5f; //0
+    float heroFeetBottomBounds = -4.5f; //-3.6
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //print("Hero moving? " + hero.isMoving + " xPos: " + hero.transform.localPosition.x);
+        if (hero.isMoving)
+        {
+            checkCameraBounds();
+            checkHeroBounds();
+        }
+	}
+
+    void checkCameraBounds()
+    {
+        Vector3 oldPos = cam.transform.localPosition;
+        if (hero.transform.localPosition.x < camLeftBound)
+        {
+            cam.transform.localPosition = new Vector3(0, oldPos.y, oldPos.z);
+        }
+        else if (hero.transform.localPosition.x > camRightBound)
+        {
+            cam.transform.localPosition = new Vector3(camRightBound, oldPos.y, oldPos.z);
+        }
+        else
+        {
+            cam.transform.localPosition = new Vector3(hero.transform.localPosition.x, oldPos.y, oldPos.z);
+        }
+    }
+
+    void checkHeroBounds()
+    {
+        if (hero.isMoving)
+        {
+            print(hero.feet.transform.position + " - " + hero.transform.localPosition);
+            Vector3 oldPos = hero.transform.localPosition;
+            if (hero.feet.position.y > heroFeetTopBounds)
+            {
+                hero.transform.localPosition = new Vector3(oldPos.x, heroTopBounds, oldPos.z);
+            }
+            else if (hero.feet.position.y < heroFeetBottomBounds)
+            {
+                hero.transform.localPosition = new Vector3(oldPos.x, heroBottomBounds, oldPos.z);
+            }
+        }
+    }
+}
--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -4,6 +4,14 @@ using System.Collections;
 public class Hero : MonoBehaviour {
     public float speed = 5;
     float multiplier = 100;
+    bool _isMoving;
+    public bool isMoving
+    {
+        get
+        {
+            return _isMoving;
+        }
+    }
 
     float adjSpeed
     {
@@ -12,38 +20,52 @@ public class Hero : MonoBehaviour {
             return speed / multiplier;
         }
     }
+    public Transform feet;
+    Animator anim;
 
 	// Use this for initialization
 	void Start () {
-	
+        _isMoving = false;
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         Vector3 currPos = transform.localPosition;
+        _isMoving = false;
 
 	    // movement
         if (Input.GetAxis("Horizontal") > 0)
         {
-            print("H+");
+            //print("H+");
             currPos.x += adjSpeed;
+            _isMoving = true;
         }
         else if (Input.GetAxis("Horizontal") < 0)
         {
-            print("H-");
+            //print("H-");
             currPos.x -= adjSpeed;
+            _isMoving = true;
         }
 
         if (Input.GetAxis("Vertical") > 0)
         {
-            print("V+");
+            //print("V+");
             currPos.y += adjSpeed;
+            _isMoving = true;
         }
         else if (Input.GetAxis("Vertical") < 0)
         {
-            print("V-");
+            //print("V-");
             currPos.y -= adjSpeed;
+            _isMoving = true;
         }
         transform.localPosition = currPos;
+
+        if (Input.GetKeyUp(KeyCode.Z))
+        {
+            print("Hi 5");
+            anim.SetTrigger("doAction");
+        }
 	}
 }
