ae4414c653dffd1bc0b05dd98cda1f8a58d894c2
Kojo Kumah <info@kojokumah.com>
2015-04-18 18:50:44 -0700

    Added GameData and Zone scripts

---
+++ Assets/GameData.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameData
+{
+    public Zone currentZone;
+
+
+
+
+
+
+
+
+
+
+
+    /* *********************************************************
+     * START - SINGLETON STUFF
+     * ********************************************************* */
+    private static GameData _instance;
+    public static GameData instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new GameData();
+            }
+            return _instance;
+        }
+    }
+
+    private GameData()
+    {
+        
+    }
+    /* *********************************************************
+     * END - SINGLETON STUFF
+     * ********************************************************* */
+
+}
---
+++ Assets/Zone.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zone {
+
+    Rect camBounds;
+    Rect heroBounds;
+
+
+    public Zone()
+    {
+        camBounds.yMin = 3;
+    }
+}
